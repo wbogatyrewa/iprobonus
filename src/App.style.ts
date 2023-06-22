5ec494cd4feb2d6e84c60073bb6c0e83332bb2a1
wbogatyrewa <veronika01-01@mail.ru>
2023-06-23 02:55:26 +0500

    add red section

---
+++ src/App.style.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const BonusContainer = styled.div`
+  position: relative;
+  padding: 0 20px;
+  min-height: 250px;
+  background: linear-gradient(180deg, #d2333e 0%, #f54b55 100%);
+  background-position: 0% 50%;
+  background-size: 100% 148px;
+  background-repeat: no-repeat;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import { Container } from "./components/Container/Container";
+import Bonus from "./components/Bonus/Bonus";
+import { BonusContainer } from "./App.style";
 
 function App() {
   return (
-    <Container>
-      <Header />
-    </Container>
+    <>
+      <Container>
+        <Header />
+      </Container>
+      <BonusContainer>
+        <Bonus total={300} burnDate="29.03" burnAmount={250} />
+      </BonusContainer>
+    </>
   );
 }
 
