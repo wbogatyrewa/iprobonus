3a185b0c03ce14e20c603d0ddaa794af234197d4
wbogatyrewa <veronika01-01@mail.ru>
2023-06-23 15:04:08 +0500

    get data from server

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,54 @@ import Header from "./components/Header/Header";
 import { Container } from "./components/Container/Container";
 import Bonus from "./components/Bonus/Bonus";
 import { BonusContainer } from "./App.style";
+import { getAccessToken } from "./api/getAccessToken";
+import { getGeneralInfo } from "./api/getGeneralInfo";
+import { parseDate } from "./scripts/parseDate";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./app/store";
+import { setUserBonus } from "./features/userBonusSlice";
+import { setAccessToken } from "./features/accessTokenSlice";
 
 function App() {
+  const accessToken = useSelector(
+    (state: RootState) => state.accessToken.value
+  );
+  const userBonus = useSelector((state: RootState) => state.userBonus);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (!accessToken) {
+      getAccessToken().then((response) => {
+        dispatch(setAccessToken(response.accessToken));
+      });
+    }
+  }, []);
+
+  React.useEffect(() => {
+    if (accessToken) {
+      getGeneralInfo(accessToken).then((data) => {
+        dispatch(
+          setUserBonus({
+            currentQuantity: data.data.currentQuantity,
+            forBurningQuantity: data.data.forBurningQuantity,
+            dateBurning: parseDate(data.data.dateBurning),
+          })
+        );
+      });
+    }
+  }, [accessToken]);
+
   return (
     <>
       <Container>
         <Header />
       </Container>
       <BonusContainer>
-        <Bonus total={300} burnDate="29.03" burnAmount={250} />
+        <Bonus
+          currentQuantity={userBonus.currentQuantity}
+          dateBurning={userBonus.dateBurning}
+          forBurningQuantity={userBonus.forBurningQuantity}
+        />
       </BonusContainer>
     </>
   );
